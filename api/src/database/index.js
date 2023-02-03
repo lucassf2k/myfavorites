01053a792921029d65d134d262bb7a859b6c8f3d
Lucas Vinicius Fernandes da Silva <lucassilved@gmail.com>
2023-02-03 20:59:16 -0300

    refactor(database): destructuring the 'rows' of the client's response

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -13,7 +13,7 @@ client.connect()
   .catch((error) => console.log(error));
 
 async function Query(query, values) {
-  const rows = await client.query(query, values);
+  const { rows } = await client.query(query, values);
   return rows;
 }
 
