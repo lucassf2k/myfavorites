3ec509d465186d759af666bd589bba6babd2c7f8
Lucas Vinicius Fernandes da Silva <lucassilved@gmail.com>
2023-02-03 12:53:44 -0300

    configuring datbase

---
+++ api/src/database/index.js
@@ -0,0 +1,20 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_DATABASE || 'myfavorites',
+});
+
+client.connect()
+  .then(() => console.log('🗃️  Database connected sucessfull'))
+  .catch((error) => console.log(error));
+
+async function Query(query, values) {
+  const rows = await client.query(query, values);
+  return rows;
+}
+
+module.exports = { Query };
