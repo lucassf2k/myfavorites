523dbc4b61da466c451e23076e552f7503e4aabc
Lucas Vinicius Fernandes da Silva <lucassilved@gmail.com>
2023-02-03 20:35:54 -0300

    feat(FavoritesRepository): finishing all methods

--- api/src/app/repositories/FavoritesRepository.js
+++ api/src/app/repositories/FavoritesRepository.js
@@ -4,9 +4,10 @@ class FavoritesRepository {
   async findAll({ orderBy = 'ASC' }) {
     const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
     const rows = await db.Query(`
-      SELECT * 
+      SELECT favorites.*, categories.name AS category_name
       FROM favorites
-      ORDER BY ${direction}
+      LEFT JOIN categories ON categories.id = favorites.id
+      ORDER BY favorites.name ${direction}
     `);
 
     return rows;
@@ -14,12 +15,65 @@ class FavoritesRepository {
 
   async findById({ id }) {
     const [row] = await db.Query(`
-      SELECT *
+      SELECT favorites.*, categories.name AS category_name
       FROM favorites
+      LEFT JOIN categories ON categories.id = favorites.category_id
       WHERE id = $1
     `, [id]);
 
     return row;
+  }
+
+  async findByName({ name }) {
+    const [row] = await db.Query(`
+      SELECT *
+      FROM favorites
+      WHERE name = $1
+    `, [name]);
+
+    const customRow = {
+      id: row.id,
+      name: row.name.toLowerCase(),
+      rating: row.rating,
+      note: row.note,
+      category_id: row.category_id,
+    };
+
+    return customRow;
+  }
+
+  async save({
+    name, rating, note, category_id,
+  }) {
+    const [row] = await db.Query(`
+      INSERT INTO favorites(name, rating, note, category_id)
+      VALUES($1, $2, $3, $4)
+      RETURNING *
+    `, [name, rating, note, category_id]);
+
+    return row;
+  }
+
+  async update(id, {
+    name, rating, note, category_id,
+  }) {
+    const [row] = await db.Query(`
+      UPDATE favorites
+      SET name = $1, rating = $2, note = $3, category_id = $4
+      WHERE id = $5
+      RETURNING *
+    `[name, rating, note, category_id, id]);
+
+    return row;
+  }
+
+  async delete({ id }) {
+    const deleteOp = await db.Query(`
+      DELETE FROM favorites
+      WHERE id = $1
+    `, [id]);
+
+    return deleteOp;
   }
 }
 
