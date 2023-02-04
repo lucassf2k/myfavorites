c7d14a6a9633e1cc9d333515593b6e740ef2d814
Lucas Vinicius Fernandes da Silva <lucassilved@gmail.com>
2023-02-03 21:55:34 -0300

    feat(routes): add routes os categories

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const { favoriteController } = require('./app/controllers/FavoriteController');
+const { categoryController } = require('./app/controllers/CategoryController');
 
 const router = Router();
 
@@ -9,5 +10,8 @@ router.post('/favorites', favoriteController.store);
 router.get('/favorites/:id', favoriteController.show);
 router.put('/favorites/:id', favoriteController.update);
 router.delete('/favorites/:id', favoriteController.delete);
+
+router.get('/categories', categoryController.index);
+router.post('/categories', categoryController.store);
 
 module.exports = { router };
