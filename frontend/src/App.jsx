a360aaae665fd4c8565de587360104de3f50b067
Lucas Silva <lucassilved@gmail.com>
2023-06-06 15:54:11 -0300

    chore: initial commit and eslint and prettier config

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyles from "./assets/styles/globals"
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Meus favoritos</h1>
+    </>
+  )
+}
+
+export default App
