b456094e7fcd7b0d9a56a1a32f63f366ef3007f0
Lucas Silva <lucassilved@gmail.com>
2023-06-13 20:59:12 -0300

    feat: add headers of acces control to allow-methods, allow-headers, max-age

--- api/src/app/helpers/allowCors.js
+++ api/src/app/helpers/allowCors.js
@@ -1,4 +1,7 @@
 export function allowCors(request, response, next) {
   response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  response.setHeader("Access-Control-Allow-Methods", "*");
+  response.setHeader("Access-Control-Allow-Headers", "*");
+  response.setHeader("Access-Control-Max-Age", "10");
   next();
 }
