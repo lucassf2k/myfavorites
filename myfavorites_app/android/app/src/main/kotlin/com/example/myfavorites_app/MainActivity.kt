4b92830ba695efaae19e2b28dd3944551397d775
Lucas Silva (lucassf2k) <lucassilved@gmail.com>
2023-07-20 13:50:29 -0300

    feat: create a project for mobile

---
+++ myfavorites_app/android/app/src/main/kotlin/com/example/myfavorites_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myfavorites_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ myfavorites_app/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:myfavorites_app/application.dart';
+
+void main() {
+  runApp(const Application());
+}
